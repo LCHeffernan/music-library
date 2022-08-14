817d86db225fd3baa4205e578edd967c3b3fff6a
Lisa Heffernan <lisaheffernan@outlook.com>
2022-08-14 13:06:59 +0100

    search feature added, tests and code

--- src/controller/artists.js
+++ src/controller/artists.js
@@ -1,20 +1,54 @@
 const getDb = require('../services/db');
 
 exports.create = async (req, res) => {
-    const db = await getDb();
-    const { name, genre } = req.body;
+  const db = await getDb();
+  const { name, genre } = req.body;
 
-    try {
-    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)',
-    [
-        name,
-        genre
+  try {
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+      name,
+      genre,
     ]);
-    
-    res.sendStatus(201);
-    } catch(err) {
-        res.sendStatus(500);
-    }
 
-    db.end();
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+
+  db.end();
 };
+
+exports.read = async (req, res) => {
+  const { artistId } = req.params;
+  const db = await getDb();
+
+  try {
+    if (!artistId) {
+      const [artists] = await db.query('SELECT * from Artist');
+      res.status(200).json(artists);
+      console.log('hello');
+    } else {
+      const [[artist]] = await db.query('SELECT * from Artist WHERE id=?', [
+        artistId,
+      ]);
+      if (artist) {
+        res.status(200).json(artist);
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  } catch (err) {
+    res.sendStatus(500);
+  }
+  db.end();
+};
+
+// exports.readById = async (req, res) =>{
+//     const { artistId } = req.params;
+//   const db = await getDb();
+//  const [ [artist] ] = await db.query('SELECT * from Artist WHERE id=?',[
+//             artistId
+//         ]);
+//         res.status(200).json(artist);
+//         db.end();
+// };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,5 +4,8 @@ const artistController = require('../controller/artists');
 const router = express.Router();
 
 router.post('/', artistController.create);
+router.get('/', artistController.read);
+// router.get('/:artistId', artistController.readById);
+router.get('/:artistId', artistController.read);
 
 module.exports = router;
